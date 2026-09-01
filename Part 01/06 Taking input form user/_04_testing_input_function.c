d046f65b6070b3c4021e05038790fd8b7599a3ef
Gian <smourya1046@gmail.com>
2026-09-01 17:40:41 +0530

    sum , and problem solving is done

--- Part 01/06 Taking input form user/_04_testing_input_function.c
+++ Part 01/06 Taking input form user/_04_testing_input_function.c
@@ -3,7 +3,8 @@
 int main() {
     int x = 5;
 
-    printf("%d\n", &x);
-
+    printf("%d\n", &x); // will print the address of x in memory
+    scanf("%d", x);
+    printf("The number you entered is : %d\n", x);
     return 0;
 }
---
+++ Part 01/06 Taking input form user/_06_predict_output.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int p, q;
+    printf("Enter values of p and q\n");
+
+    scanf(" %d %d", &p, &q);
+    printf("p = %d q = %d\n", p, q);
+    return 0;
+}
