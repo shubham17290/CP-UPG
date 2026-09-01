48ba76038488b7f28284aaa4302593b78e1169d9
Gian <smourya1046@gmail.com>
2026-09-01 17:15:27 +0530

    input program 2 is done

--- Part 01/06 Taking input form user/_02_program.c
+++ Part 01/06 Taking input form user/_02_program.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-   
+int main() {
+    float r;
+    printf("enter the value of radius : ");
+    scanf("%f", &r);
+    float pi = 3.14;
+    float area = pi * r * r;
+    printf("Area of circle is: %.2f\n", area);
 
     return 0;
 }
