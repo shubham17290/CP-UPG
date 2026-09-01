2023a075126e0d955ab4789bed3d8c81caf6cc2b
Gian <smourya1046@gmail.com>
2026-09-01 14:47:30 +0530

    done with the concept of the basics of the varibales  , like declaration , initialization , updation , and increment and decrement

---
+++ Part 01/02 Variables/_01_declaration.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    x = 7;
+    printf("%d\n", x);
+
+    x = 9;
+    printf("%d\n", x);
+    return 0;
+}
---
+++ Part 01/02 Variables/_02_var_declartion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    x = 7;
+    printf("%d\n", x);
+    x = x + 6;
+    printf("%d\n", x); // x= 13
+    x = x - 20;        // x= -7
+    printf("%d\n", x); // x = -7
+    return 0;
+}
