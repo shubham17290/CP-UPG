ac59da15262e53ad9941821ab66064568e40308a
Gian <smourya1046@gmail.com>
2026-09-04 15:01:10 +0530

    odd and even number

---
+++ Part 02 Conditional Statement(If else) in one go/oddeven_1.c
@@ -0,0 +1,18 @@
+//  Take input from, user and print the number is even or odd
+
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("The number is even.\n");
+    }
+    if (number % 2 != 0) {
+        printf("The number is odd.\n");
+    }
+
+    return 0;
+}
