90c226ac1840f5d959cee8cda0a158ae86329b03
Gian <smourya1046@gmail.com>
2026-09-06 11:05:27 +0530

    c functions

---
+++ Part 05 Functions and Pointers/basicfunction.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void greet() {
+    printf("How Buddy , good morning !\n");
+    printf("How are you ?\n");
+}
+int main() {
+    greet();
+    greet();
+    greet();
+
+    return 0;
+}
